6eb28af641b12147fbd39427f5c99e3f1e1ddc7f
David M Freeman <davidfreeman9@gmail.com>
2016-02-16 13:14:08 -0500

    Ok, so this seems to work quite a bit better. I first changed the approach to applying the subdivision function, which seemed to have little impact. The main difference seems to stem from the use of PolyCollection instead of PathPatch when drawing the figure at the end of the script. Once I made this change, suddenly higher iterations were completed in a reasonable amount of time.

--- pinwheel_color_summer_faster.py
+++ pinwheel_color_summer_faster.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 from matplotlib.path import Path
-from matplotlib.patches import PathPatch
+from matplotlib.collections import PolyCollection
 from mpmath import *
 
 plt.figure(figsize=(1,0.6),dpi=800)
@@ -30,7 +30,6 @@ filename = 'pinwheel(%s,%s,%s)_summer.png' %(p,q,m)
 # Now we calculate the proportions of the triangles based on (p,q). The number b provides
 # the base of a triangle, and the number a provides the height. The hypotenuse equals 1.
 
-print('calculating proportions of the tilings')
 mp.dps = 10
 mp.pretty = True
 f = lambda x: (0.5**p)*(1-x**2)**(0.5*p)-x**q
@@ -53,8 +52,7 @@ sizes = [1]
 # Now we define the subdivision rule.
 
 def subdivide(largest):
-	print('subdividing triangle')
-	result = []
+	result=[]
 	for size,A,B,C in largest:
 		P = A + (C-A)*(0.5*b**2)
 		Q = A + (C-A)*(b**2)
@@ -66,13 +64,11 @@ def subdivide(largest):
 # Now we apply the subdivision rule. Note the need for a bit of rounding!
 
 for i in xrange(m):
-	print('apply our subdivision rule ' + str(i))
 	n = max(sizes)
-	for size,A,B,C in triangles:
-		if round(size,4) == round(n,4):
-			triangles.extend(subdivide([(size,A,B,C)]))
-			sizes.extend([size*c,size*a])
-			print("length of triangles is " + str(len(triangles)))
+	largest = [(size,A,B,C) for (size,A,B,C) in triangles if round(size,5) == round(n,5)]
+	triangles.extend(subdivide(largest))
+	triangles = [(size,A,B,C) for (size,A,B,C) in triangles if round(size,5) != round(n,5)]
+	sizes.extend([n*c,n*a])
 	sizes = [x for x in sizes if round(x,5) != round(n,5)]
 
 # Now we clean up the sizes list in order to color the tiling
@@ -82,22 +78,21 @@ final_sizes = []
 for i in xrange(r):
 	if sizes:
 		final_sizes.extend([sizes[0]])
-		sizes = [x for x in sizes if round(x,4) != round(sizes[0],4)]
+		sizes = [x for x in sizes if round(x,5) != round(sizes[0],5)]
 
 # Now we set the color scheme and draw the triangles
 
 cmap = mpl.cm.summer
 
-def DrawFigure(triangles):
-    print('draw the figure')
-    for size,A,B,C in triangles:
-        print('looping through triangles')
-        for i in xrange(len(final_sizes)):
-		print('looping through sizes')
-        	if round(size,4) == round(final_sizes[i],4):
-        		plt.gca().add_patch(PathPatch(Path([C,A,B,C],[Path.MOVETO]+[Path.LINETO]*3),facecolor=cmap(i/float(r)),edgecolor='#000000',joinstyle='round',linewidth=0))
-    plt.savefig(filename, dpi=2400, format='png')
-    # plt.show()
+def approx_equal(a, b):
+     return abs(a - b) <= max(abs(a),abs(b))*(1e-5)
 
+def DrawFigure(triangles):
+	for i in xrange(len(final_sizes)):
+		print('current ' + str(i))
+		current = [(A,B,C) for (size,A,B,C) in triangles if approx_equal(size,final_sizes[i])]
+		plt.gca().add_collection(PolyCollection(current,facecolor=cmap(i/float(r)),edgecolor='none'))
+	plt.savefig(filename, dpi=3600, format='png')
+	#plt.show()
 
 DrawFigure(triangles)
